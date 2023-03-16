6b1f9cd2980e423ef54c1fe9673323a59ea0cc20
MMuttel <BuckyFeedback@gmail.com>
2023-03-16 14:30:51 +0000

    Update simple arduino code to activate a touch screen.ino

--- simple arduino code to activate a touch screen.ino
+++ simple arduino code to activate a touch screen.ino
@@ -1,17 +1,17 @@
-// the first inspiration for this project started with this youtube 
+// The first inspiration for this project started in 2019 with this youtube:
 //        Arduino Plays Piano Tiles tutorial
 //        https://www.youtube.com/watch?v=8hlQ0MiowN8
 //
-// there are many ways to achieve this process
-// there are lots of things to sort out in this project
-// there needs to be a wire or wires connected between a touch screen and a controller (in this case the controller is an arduino unit)
-// the connection to the touch screen must fulfill certian parameters
-//      1. there needs to be about a 1cm square (or round) conductive touch point attached to the touch screen
-//      2. the wire connecting the touch point to the controller should be fairly short (as short as possible) 
-//      3. the wire should be insulated
-//      4. if two or more touch points are used, the wires MUST be seperated by a cm or more along their path
-//            if the wires are too close together, they will effect one another and 'funny things' may happen 
-//      5. regarding the youtube video above, there are other ways than using relays to 'artifically touch' the touch screen.
+// There are many ways to achieve this process.
+// There were lots of things to sort out in this project.
+// There needs to be a wire or wires connected between a touch screen and a controller (in this case the controller is an arduino unit).
+// The connection to the touch screen must fulfill certian parameters:
+//      1. There needs to be about a 1cm square (or round) conductive touch point attached to the touch screen.
+//      2. The wire connecting the touch point to the controller should be fairly short (as short as possible). 
+//      3. The wires should be insulated.
+//      4. If two or more touch points are used, the wires MUST be seperated by a cm or more along their path.
+//            If the wires are too close together, they will effect one another and 'funny things' may happen.
+//      5. Regarding the youtube video above, there are other ways than using relays to 'artifically touch' the touch screen.
 //            I stopped using relays after about a month of experiments. This project does NOT include relays.
 //
 //    OK let's get started
