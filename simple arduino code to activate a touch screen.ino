3e23e3a2d08741de85f35295464ac000283d47ba
MMuttel <BuckyFeedback@gmail.com>
2023-03-16 14:16:43 +0000

    added introduction

---
+++ simple arduino code to activate a touch screen.ino
@@ -0,0 +1,36 @@
+// the first inspiration for this project started with this youtube 
+//        Arduino Plays Piano Tiles tutorial
+//        https://www.youtube.com/watch?v=8hlQ0MiowN8
+//
+// there are many ways to achieve this process
+// there are lots of things to sort out in this project
+// there needs to be a wire or wires connected between a touch screen and a controller (in this case the controller is an arduino unit)
+// the connection to the touch screen must fulfill certian parameters
+//      1. there needs to be about a 1cm square (or round) conductive touch point attached to the touch screen
+//      2. the wire connecting the touch point to the controller should be fairly short (as short as possible) 
+//      3. the wire should be insulated
+//      4. if two or more touch points are used, the wires MUST be seperated by a cm or more along their path
+//            if the wires are too close together, they will effect one another and 'funny things' may happen 
+//      5. regarding the youtube video above, there are other ways than using relays to 'artifically touch' the touch screen.
+//            I stopped using relays after about a month of experiments.
+//
+//    OK let's get started
+//
+//  MATERIALS NEEDED
+//      CONTROLLER (Arduino)
+//      TOUCH POINT tape
+//      WIRE
+//
+//  THE CONTROLLER
+//  An arduino unit. Any arduino will work but this sketch will involve an ARDUINO NANO, I have also used ARDUINO UNO, and ARDUINO MEGA and they work well.
+//
+//  THE TOUCH POINTS
+//  The touch point pads I have found to be the most effective, are made from aluminium adhesive tape. This adhesive is very inexpensive, 
+//  widely available, and marketed to secure duct seams.
+//
+//  THE WIRES
+//  Any small guage insulated wire will work. One consideration is to match the guage to the similar size that will attact to the input/output
+//  pin sockets of the arduino.
+//
+//
+//  code and more instructions coming soon
